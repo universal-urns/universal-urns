8f125ccc8040a613e5b575a053593fae392cfcd8
Lyle Kobus <ljkob@HR-SUR.ARM.local>
2025-05-27 17:15:51 -0400

    Fresh secure version with Stripe secret removed

---
+++ checkout.js
@@ -0,0 +1,36 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  try {
+    const { amount } = JSON.parse(event.body);
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'Urn Order',
+            },
+            unit_amount: amount,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'https://universal-urns.netlify.app/success',
+      cancel_url: 'https://universal-urns.netlify.app/cancel',
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ id: session.id }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
